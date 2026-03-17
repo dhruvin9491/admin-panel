3259b72c0e503c5334dbab379d3206ac04e8bf51
dhruvin9491 <dhruvin9491@gmail.com>
2026-03-17 13:54:37 +0530

    make uploadImage.js gitignore

--- src/helper/UploadImage.js
+++ src/helper/UploadImage.js
@@ -4,10 +4,10 @@ export const uploadImage = async (file) => {
     const formData = new FormData();
 
     formData.append("file", file);
-    formData.append("upload_preset", "PRESET_NAME");
-    formData.append("folder", "FOLDER_NAME");
+    formData.append("upload_preset", "foodyassets");
+    formData.append("folder", "products");
 
-    const response = await axios.post("https://api.cloudinary.com/v1_1/YOUR_CLOUD_NAME/image/upload", formData);
+    const response = await axios.post("https://api.cloudinary.com/v1_1/deiofdjcn/image/upload", formData);
 
     return response.data.secure_url;
 }
